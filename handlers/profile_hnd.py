a068330ad6c1fd85a69c41053fe5294c1ed1e3ef
Ranver <qwertyggm.v.p@gmail.com>
2023-08-07 22:04:25 +0300

    fixed text coding in profile_hnd.py top_up

--- handlers/profile_hnd.py
+++ handlers/profile_hnd.py
@@ -108,8 +108,8 @@ async def top_up(callback: types.CallbackQuery):
             await bot.send_message(callback.from_user.id,
                                    f'''
 Введите сумму для пополнения, минимальная сумма - 5 рублей
-<code>Для пополнения по номеру карты(сбербанк, тинькофф) напишите в личные сообщения @AsuraStore_helper.</code>
-    
+<code>Для пополнения по номеру карты(сбербанк, тинькофф)</code> напишите в личные сообщения @AsuraStore_helper.
+
 Через платежную систему(Юмани) комиссия 3% с банковских карт, с баланса Юмани комиссии нет. 
     ''',
                                    reply_markup=kb.choose_insert, parse_mode=types.ParseMode.HTML)
